52568c421265e0c2b262a2f517bebdc6a8a68ca1
Thomas Rasmussen <thomas.bregentved@gmail.com>
2010-01-30 23:26:39 +0100

    Added code to get rid of unwanted chars and uppercase input
    We only accept: A-Z, a-z and 0-9
    
    The morsecode lookup table only have uppercase letters, so I
    change all letters to uppercase.

--- morse.sh
+++ morse.sh
@@ -7,6 +7,8 @@ do
     if [ $WORDS -gt 0 ]; then
         echo -n '//'
     fi
+    x=`echo $x | tr -cd '[[a-zA-z0-9]]'`
+    x=`echo $x | tr '[:lower:]' '[:upper:]'`
     echo $x | fold -w1 | sed -f alphabet-morse.sed | tr '\n' '/';
     let WORDS=WORDS+1;
 done
