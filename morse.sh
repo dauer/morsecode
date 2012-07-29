47c0d533ba9e1f6f2342b7d51ae6d3fab2663df8
Thomas Rasmussen <thomas.bregentved@gmail.com>
2012-07-30 00:55:14 +0200

    morse.sh now looks for alphabet file in same folder

--- morse.sh
+++ morse.sh
@@ -2,7 +2,7 @@
 
 WORDS=0
 STR=''
-
+ALPHA=`dirname $0`/alphabet-morse.sed
 for x in `cat`
 do
     if [ $WORDS -gt 0 ]; then
@@ -10,7 +10,7 @@ do
     fi
     x=`echo $x | tr -cd '[[a-zA-z0-9]]'`
     x=`echo $x | tr '[:lower:]' '[:upper:]'`
-    STR=$STR`echo $x | fold -w1 | sed -f alphabet-morse.sed | tr '\n' '/'`;
+    STR=$STR`echo $x | fold -w1 | sed -f $ALPHA | tr '\n' '/'`;
     let WORDS=WORDS+1;
 done
 
